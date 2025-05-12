af08d0fe17e6121beda7e514959ce4d5b7a816b3
BhanuPrasad237 <139207188+BhanuPrasad237@users.noreply.github.com>
2025-05-12 10:42:33 +0530

    added models and db configuration in settings

--- bus/bus_app/models.py
+++ bus/bus_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Bus(models.Model):
+    bus_name = models.CharField(max_length=100)
+    number = models.CharField(max_length=100, unique=True)
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.bus_name}-{self.number}"
+
+
+class Seat(models.Model):
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    seat_number = models.CharField(max_length=10)
+    is_booked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.seat_number
