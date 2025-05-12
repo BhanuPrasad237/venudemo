90358a70c4c5870c9fb747ac4d3a247ba2ac9b12
BhanuPrasad237 <139207188+BhanuPrasad237@users.noreply.github.com>
2025-05-12 11:49:11 +0530

    added booking model and user register serializer

--- bus/bus_app/models.py
+++ bus/bus_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -8,7 +9,7 @@ class Bus(models.Model):
     number = models.CharField(max_length=100, unique=True)
     origin = models.CharField(max_length=100)
     destination = models.CharField(max_length=100)
-    no_of_seats=models.PositiveIntegerField()
+    no_of_seats = models.PositiveIntegerField()
     start_time = models.TimeField()
     end_time = models.TimeField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
@@ -24,3 +25,13 @@ class Seat(models.Model):
 
     def __str__(self):
         return self.seat_number
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    booking_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}-{self.bus.bus_name}-{self.seat.seat_number}"
--- bus/bus_app/serializers.py
+++ bus/bus_app/serializers.py
@@ -1,5 +1,22 @@
 from rest_framework import serializers
 from .models import Bus, Seat
+from django.contrib.auth.models import User
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=100, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
 
 
 class BusSerializer(serializers.ModelSerializer):
@@ -11,4 +28,4 @@ class BusSerializer(serializers.ModelSerializer):
 class SeatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Seat
-        fields = ["id","seat_number","is_booked"]
+        fields = ["id", "seat_number", "is_booked"]
